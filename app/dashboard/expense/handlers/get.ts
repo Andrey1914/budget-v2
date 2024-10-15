0efef369acf64eb7d4396367bdcbd9ed9c4668ba
Andrey1914 <anlikajw@gmail.com>
2024-10-15 19:29:31 +0200

    updated get function, used axios

--- app/dashboard/expense/handlers/get.ts
+++ app/dashboard/expense/handlers/get.ts
@@ -1,9 +1,10 @@
+import axios from "axios";
+
 export const fetchCategories = async () => {
   try {
-    const res = await fetch("/api/expense/categories");
-    if (!res.ok) throw new Error("Failed to fetch categories");
-    const data = await res.json();
-    return data || [];
+    const res = await axios.get("/api/expense/categories");
+
+    return res.data || [];
   } catch (error) {
     console.error("Error fetching categories:", error);
     throw error;
