12ae48099bc39a20e9b304051cd4cab740e4e7f5
Andrey1914 <anlikajw@gmail.com>
2025-05-09 18:57:35 +0200

    replaced a non-existent route

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
@@ -15,10 +15,15 @@ import FAQ from "@/components/faq/FAQ";
 // import AverageRating from "@/components/Review/AverageRating";
 import Feature from "@/components/Features/Features";
 import { GetStartedButton } from "@/app/styles/Buttons";
+import AuthTabsModal from "@/components/Auth/AuthModal";
 
 const Landing: React.FC = () => {
   const { data: session, status } = useSession();
   const router = useRouter();
+
+  const [authModalOpen, setAuthModalOpen] = useState(false);
+  const handleOpenAuthModal = () => setAuthModalOpen(true);
+  const handleCloseAuthModal = () => setAuthModalOpen(false);
 
   const theme = useTheme();
 
@@ -26,132 +31,136 @@ const Landing: React.FC = () => {
     if (status === "authenticated") {
       router.push("/dashboard/reviews");
     } else {
-      router.push("/auth/register");
+      setAuthModalOpen(true);
     }
   };
 
-  const handleNavigation = () => {
-    router.push("/auth/register");
-  };
-
   return (
-    <MainContainer>
-      <Box component="section" title="hero">
-        <Hero />
-      </Box>
+    <>
+      <MainContainer>
+        <Box component="section" title="hero">
+          <Hero />
+        </Box>
 
-      <Box component="section" title="advantages" sx={{ py: "72px" }}>
-        {/* <Advantages /> */}
-        <AdvantagesCarousel />
-      </Box>
+        <Box component="section" title="advantages" sx={{ py: "72px" }}>
+          {/* <Advantages /> */}
+          <AdvantagesCarousel />
+        </Box>
 
-      {/* Основные функции */}
-      <Box component="section" title="main-features">
-        <Feature />
-      </Box>
+        {/* Основные функции */}
+        <Box component="section" title="main-features">
+          <Feature />
+        </Box>
 
-      {/* Отзывы и кейсы успеха */}
-      <Box
-        component="section"
-        title="reviews"
-        sx={{
-          py: 6,
-          px: 2,
-          background: theme.palette.gradients.reviews,
-        }}
-      >
-        <Container maxWidth="md">
-          <Box sx={{ py: 4 }}>
-            <Typography
-              variant="h2"
-              component="h2"
-              gutterBottom
-              sx={{
-                fontSize: theme.typography.fontSizes[6],
-                fontWeight: theme.typography.fontWeightLight,
-              }}
-            >
-              We hope you enjoy our app!
-            </Typography>
-            <Typography
-              variant="h4"
-              component="p"
-              gutterBottom
-              sx={{ fontSize: theme.typography.fontSizes[4] }}
-            >
-              Sign in and leave your feedback. It helps us to became better!
-            </Typography>
-            {/* <AverageRating /> */}
-          </Box>
-
-          <Box sx={{ py: 4, width: "750px" }}>
-            <ReviewsCarousel />
-          </Box>
-
-          <Box sx={{ py: 4 }}>
-            <Box
-              component="div"
-              sx={{
-                display: "flex",
-                ml: "auto",
-                alignItems: "end",
-                flexDirection: "column",
-                gap: "1rem",
-                maxWidth: "200px",
-              }}
-            >
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={handleReviewClick}
+        {/* Отзывы и кейсы успеха */}
+        <Box
+          component="section"
+          title="reviews"
+          sx={{
+            py: 6,
+            px: 2,
+            background: theme.palette.gradients.reviews,
+          }}
+        >
+          <Container maxWidth="md">
+            <Box sx={{ py: 4 }}>
+              <Typography
+                variant="h2"
+                component="h2"
+                gutterBottom
+                sx={{
+                  fontSize: theme.typography.fontSizes[6],
+                  fontWeight: theme.typography.fontWeightLight,
+                }}
               >
-                <Send sx={{ mr: 2 }} />
-                Send feedback
-              </Button>
-
-              <Link
-                href="reviews"
-                style={{ display: "flex", textDecoration: "none" }}
+                We hope you enjoy our app!
+              </Typography>
+              <Typography
+                variant="h4"
+                component="p"
+                gutterBottom
+                sx={{ fontSize: theme.typography.fontSizes[4] }}
               >
-                <Typography
-                  variant="body2"
-                  sx={{
-                    fontSize: "14px",
-                    p: 1,
-                    color: theme.palette.text.secondary,
-                    border: `2px solid ${theme.palette.text.secondary}`,
-                    borderRadius: theme.spacing(1),
-                  }}
+                Sign in and leave your feedback. It helps us to became better!
+              </Typography>
+              {/* <AverageRating /> */}
+            </Box>
+
+            <Box sx={{ py: 4, width: "750px" }}>
+              <ReviewsCarousel />
+            </Box>
+
+            <Box sx={{ py: 4 }}>
+              <Box
+                component="div"
+                sx={{
+                  display: "flex",
+                  ml: "auto",
+                  alignItems: "end",
+                  flexDirection: "column",
+                  gap: "1rem",
+                  maxWidth: "200px",
+                }}
+              >
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleReviewClick}
                 >
-                  All reviews
-                </Typography>
-              </Link>
+                  <Send sx={{ mr: 2 }} />
+                  Send feedback
+                </Button>
+
+                <Link
+                  href="reviews"
+                  style={{ display: "flex", textDecoration: "none" }}
+                >
+                  <Typography
+                    variant="body2"
+                    sx={{
+                      fontSize: "14px",
+                      p: 1,
+                      color: theme.palette.text.secondary,
+                      border: `2px solid ${theme.palette.text.secondary}`,
+                      borderRadius: theme.spacing(1),
+                    }}
+                  >
+                    All reviews
+                  </Typography>
+                </Link>
+              </Box>
+            </Box>
+          </Container>
+        </Box>
+
+        {/* FAQ */}
+        <Container maxWidth="md">
+          <Box component="section" title="FAQ" sx={{ p: 4 }}>
+            <Typography variant="h4">FAQs</Typography>
+            <Box sx={{ pt: 4 }}>
+              <FAQ />
             </Box>
           </Box>
-        </Container>
-      </Box>
 
-      {/* FAQ */}
-      <Container maxWidth="md">
-        <Box component="section" title="FAQ" sx={{ p: 4 }}>
-          <Typography variant="h4">FAQs</Typography>
-          <Box sx={{ pt: 4 }}>
-            <FAQ />
+          {/* Призыв к действию */}
+          <Box component="section" sx={{ p: 4 }}>
+            <GetStartedButton
+              colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+              onClick={handleOpenAuthModal}
+            >
+              <span className="text-content">Get Started</span>
+              <span className="gradient-overlay"></span>
+            </GetStartedButton>
           </Box>
-        </Box>
+        </Container>
+      </MainContainer>
 
-        {/* Призыв к действию */}
-        <Box component="section" sx={{ p: 4 }}>
-          <GetStartedButton
-            colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
-            onClick={handleNavigation}
-          >
-            <span className="text-content">Get Started</span>
-            <span className="gradient-overlay"></span>
-          </GetStartedButton>
-        </Box>
-      </Container>
-    </MainContainer>
+      <AuthTabsModal
+        open={authModalOpen}
+        onClose={handleCloseAuthModal}
+        initialTab={0}
+      />
+    </>
   );
 };
 
